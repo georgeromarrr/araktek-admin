f72787da539ddcfcf072273bf3964a319ce83ca1
georgeromarrr <romar.doinog@gmail.com>
2022-08-18 15:23:42 +0800

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'desktop': {'min': '1201px'},
+      // => @media (min-width: 1200px) { ... }
+
+      'laptop': {'min': '768px', 'max': '1200px'},
+      'tablet': {'min': '475px', 'max': '768px'},
+      'mobile': {'max': '475px'},
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
