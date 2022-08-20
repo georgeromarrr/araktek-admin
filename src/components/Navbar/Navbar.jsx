e7eabd0fdd4cab5a92524de00d65bd613b9dd6cd
georgeromarrr <romar.doinog@gmail.com>
2022-08-20 17:52:32 +0800

    created navbar ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
+import DateTime from "../DateTime/DateTime";
+import {
+  RightNavbar,
+  BlackLogo,
+  WhiteLogo,
+  UserMenu,
+} from "./Navbar_Components";
 import DarkMode from "../DarkMode/DarkMode";
-import { BlackLogo, WhiteLogo, UserMenu } from "./Navbar_Components";
 
 const Navbar = () => {
   const [themeMode, setThemeMode] = useState(false);
@@ -13,18 +19,11 @@ const Navbar = () => {
       setThemeMode(false);
     }
   }, []);
-
   return (
-    <div className="border border-black inset-x-0 rounded-md mt-2 mr-2 bg-gray-50 dark:bg-neutral-900 dark:border-white dark:text-white">
-      <nav className="py-4 mx-12 flex justify-between">
-        {themeMode ? <WhiteLogo /> : <BlackLogo />}
-        <div className="flex self-center items-center gap-x-1">
-          <p className="dark:text-white">Welcome,</p>
-          <UserMenu username="Romar George Doinog" />
-          <DarkMode onBtnClick={() => setThemeMode(!themeMode)} />
-        </div>
-      </nav>
-    </div>
+    <nav className="w-full flex">
+      <DateTime />
+      <RightNavbar />
+    </nav>
   );
 };
 
--- src/components/Navbar/Navbar_Components.jsx
+++ src/components/Navbar/Navbar_Components.jsx
@@ -25,7 +25,7 @@ export const UserMenu = (props) => {
     <div className="relative">
       <button
         type="button"
-        className="text-black flex items-center hover:bg-neutral-300 p-2 rounded-lg relative dark:text-white dark:hover:bg-neutral-700"
+        className="text-black flex items-center hover:bg-neutral-200 p-2 rounded-lg relative dark:text-white dark:hover:bg-neutral-700"
         onClick={() => setOpen(!isOpen)}
       >
         <p className="font-semibold pr-1 dark:text-white">{props.username}</p>
@@ -37,15 +37,46 @@ export const UserMenu = (props) => {
         <div className="text-black absolute border border-black bg-white right-0 top-11 w-full rounded-sm divide-y divide-gray-400 shadow-xl px-1 dark:border-white dark:bg-neutral-900 dark:text-white">
           <p className="text-sm italic p-4">Owner</p>
           <ul className="p-2 text-sm">
-            <li className="font-semibold cursor-pointer hover:bg-gray-300 hover:text-gray-700 p-2 rounded-sm dark:hover:bg-neutral-700 dark:hover:text-neutral-200">
+            <li className="font-semibold cursor-pointer hover:bg-neutral-200 hover:text-neutral-700 p-2 rounded-sm dark:hover:bg-neutral-700 dark:hover:text-neutral-200">
               Settings
             </li>
-            <li className="font-semibold cursor-pointer hover:bg-gray-300 hover:text-gray-700 p-2 rounded-sm dark:hover:bg-neutral-700 dark:hover:text-neutral-200">
+            <li className="font-semibold cursor-pointer hover:bg-neutral-200 hover:text-neutral-700 p-2 rounded-sm dark:hover:bg-neutral-700 dark:hover:text-neutral-200">
               Log Out
             </li>
           </ul>
         </div>
       )}
+    </div>
+  );
+};
+
+// navbar
+export const RightNavbar = () => {
+  const [themeMode, setThemeMode] = useState(false);
+  const userTheme = JSON.parse(localStorage.getItem("theme"));
+
+  useEffect(() => {
+    if (userTheme === "dark") {
+      setThemeMode(true);
+    } else {
+      setThemeMode(false);
+    }
+  }, []);
+
+  return (
+    <div className="w-full mr-1 my-1 border border-black rounded-md bg-gray-50 dark:bg-neutral-900 dark:border-white dark:text-white">
+      <div className="flex justify-between my-4 mx-12">
+        <div className="self-center">
+          {themeMode ? <WhiteLogo /> : <BlackLogo />}
+        </div>
+        <div className="self-center">
+          <div className="flex items-center gap-x-1">
+            <p className="dark:text-white">Welcome,</p>
+            <UserMenu username="Romar George Doinog" />
+            <DarkMode onBtnClick={() => setThemeMode(!themeMode)} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
