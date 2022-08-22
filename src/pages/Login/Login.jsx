95c6daa8a965e9ce677427a127b389d0f611fa07
jryambao <johnralph266@gmail.com>
2022-08-22 13:30:36 +0800

    added login function

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { EyeIcon, EyeOffIcon } from "../../assets/Icons/Icons";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 import {
   LoginHeader,
   LoginForm,
@@ -12,15 +15,56 @@ import {
 const Login = () => {
   const [showPass, setShowPass] = useState(false);
 
+  let navigate = useNavigate();
+  const [login, setLogin]=useState({
+  email: '',
+  password:'',
+  error_list: [],
+  error_wrong: [],
+})
+
+  const handleInput =(e)=>{
+  e.persist();
+  setLogin({...login, [e.target.name]: e.target.value });
+}
+  const loginSubmit=(e)=>{
+  e.preventDefault();
+  const data= {
+    email: login.email,
+    password: login.password,
+  }
+  axios.get('/sanctum/csrf-cookie').then(response => {
+    axios.post(`api/login`, data).then(res =>{
+      if(res.data.status === 200){
+        localStorage.setItem('auth_token', res.data.token);
+        localStorage.setItem('auth_name', res.data.username)
+      //   swal("Success", res.data.message, "success")
+        navigate("/", { replace: true });
+
+      }
+      else if(res.data.status === 401){
+      //   swal("Warning", res.data.message, "Warning")
+        setLogin({...login, error_wrong: res.data.message})
+      }
+      else{
+        setLogin({...login, error_list: res.data.validation_errors });
+
+      }
+    });
+});
+
+}
   return (
     <div className="relative">
       <Background />
       <LoginForm>
         <div className="p-4 w-[46vh] mx-auto">
           <LoginHeader />
-          <form className="flex flex-col gap-4 pb-4">
+          <form onSubmit={loginSubmit} className="flex flex-col gap-4 pb-4">
+
             <div className="py-3">
               <input
+                onChange={handleInput} value={login.email}
                 className={InputClass}
                 name="email"
                 type="email"
@@ -31,6 +75,7 @@ const Login = () => {
 
             <div className="relative">
               <input
+                onChange={handleInput}  value={login.password}
                 className={InputClass}
                 name="password"
                 type={showPass ? `text` : `password`}
